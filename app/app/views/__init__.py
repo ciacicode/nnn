8e8e5f288296fa1425730b29d5d1ac9297c3a546
shreyas08 <shreyas@mediaiqdigital.com>
2018-07-14 02:19:53 +0530

    Upload using FrontEnd

--- app/app/views/__init__.py
+++ app/app/views/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-
-
+import os
+from .pdftotext import ConvertPdfToText
 from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
 from .resume_form import Resume
 
 bp = Blueprint('views', __name__, template_folder='templates')
+MYDIR = os.path.dirname(__file__)
 
 @bp.route('/')
 def show():
@@ -20,8 +21,14 @@ def unbias():
     form = Resume(request.form)
     if form.validate_on_submit():
         # show resume_result
-        result = {"id": 0,"skills": [ "Python", "Css"],"experience": ["Google", "Import.io"],"education": ["University of Federico II, Naples"]}
-        return render_template('resume_result.html', result=result)
+        #unbiased = {"id": 0,"skills": [ "Python", "Css"],"experience": ["Google", "Import.io"],"education": ["University of Federico II, Naples"]}
+        file = request.files['resume']
+        #file is a binary from the post request
+        #filename = secure_filename(file.filename)
+        file.save(os.path.join(MYDIR+'/static/', file.filename))
+        unbiased = ConvertPdfToText(MYDIR+'/static/'+file.filename)
+
+        return render_template('resume_result.html', result=unbiased)
     else:
         #it's a get request
         return render_template('resume_upload.html', form=form)
