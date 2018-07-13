0a47f8c67df3bc4d268bc4186c6ab44455ca3f0f
Shraddha Mehta <shraddha@mediaiqdigital.com>
2018-07-14 00:39:12 +0530

    adding disc score calculation methodology

--- app/app/v1/api/diversity_score.py
+++ app/app/v1/api/diversity_score.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, print_function
 
 from flask_wtf import Form
-from flask import request, g
+from flask import request, g, jsonify
 from wtforms import SubmitField, TextAreaField, validators, ValidationError
 from watson_developer_cloud import PersonalityInsightsV3
 from datetime import datetime, date
@@ -40,12 +40,11 @@ def get_personality_insights(profile):
     year = today.year
     month = today.month
     personality = personality_insights.profile(profile, content_type='text/plain;charset=utf-8',raw_scores=True, consumption_preferences=True)
-    
     #serialise to string and then to object
     result = json.loads(json.dumps(personality))
     return result
 
-def generate_data(insights, category='needs'):
+def generate_data(insights, category='personality'):
     """
     insights: as json from watson
     category: one between needs (default), consumption_preferences, values, personality
@@ -92,8 +91,42 @@ def generate_all_data(insights):
 class DiversityScore(Resource):
 
     def post(self):
-        #print(g.form)
-        
+        if 'file' not in request.files:
+            print ('No file part')
+            #if not, redirect maybe to GET??? to do
+            return redirect(request.url)
+        else:
+            #file = request.files['file']
+            #file = "some random text"
+            #file is a binary from the post request
+            #you must return an object that matches the schemas description in schemas.py
+            f = open('cv.txt','r')
+            data = f.read()
 
-        return {}, 200, None
+            insights = get_personality_insights(data)
+            data = generate_data(insights, category='personality')
+            labels = data.get('labels')
+            raw_scores = data.get('raw_scores')
+            for index, label in enumerate(labels):
+                if label == 'Openness':
+                    openness_score = raw_scores[index]
+                if label == 'Conscientiousness':
+                    ocean_conscient_score = raw_scores[index]
+                if label == 'Extraversion':
+                    extraversion_score = raw_scores[index]
+                if label == 'Agreeableness':
+                    agreeableness_score = raw_scores[index]
+                if label == 'Emotional range':
+                    emotional_score = raw_scores[index]
+            #print(openness_score, conscient_score, extraversion_score, agreeableness_score, emotional_score)
+            dominance_score = -0.023*extraversion_score + 0.126*openness_score - 0.278*agreeableness_score + 0.039*ocean_conscient_score - 0.297*emotional_score
+            influence_score = 0.383*extraversion_score + 0.251*openness_score + 0.114*agreeableness_score -0.196*ocean_conscient_score + 0.032*emotional_score
+            steadiness_score = -0.063*extraversion_score - 0.234*openness_score + 0.308*agreeableness_score - 0.054*ocean_conscient_score - 0.275*emotional_score
+            disc_conscient_score = -0.3*extraversion_score + -0.175*openness_score - 0.157*agreeableness_score + 0.185*ocean_conscient_score + -0.008*emotional_score
+            
+            return {"personality" : {"dominance" : dominance_score, 
+                                     "influence" : influence_score, 
+                                     "steadiness" : steadiness_score, 
+                                     "conscient" : disc_conscient_score}}, 200, None
+        
     
