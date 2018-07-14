823777e14aa85c55c34446ec5203d0faba22574f
shreyas08 <shreyas@mediaiqdigital.com>
2018-07-14 17:27:06 +0530

    jacquard index score unbias

--- app/app/views/unBiased.py
+++ app/app/views/unBiased.py
@@ -5,16 +5,11 @@ import random
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import docx2txt
-from model_ouput_keywords import var
-from collections import defaultdict
-from nltk.tokenize import sent_tokenize
 
 def ConvertPdfToText(file):
     #pdfFileObj = open('./static/test.pdf', 'rb')
     text = ""
     data = {}
-    output_dict={}
-    new_output_dict = defaultdict(list)
     keys = ["skills", "experience", "education", "awards"]
 
     for i in keys:
@@ -37,35 +32,16 @@ def ConvertPdfToText(file):
     else:
         text = "Invalid File Type\n"
 
-
-
-    new_text_temp = text.lower()
-    new_text = new_text_temp.split('\n')
-
     cleanString = re.sub(r"[^a-zA-Z0-9]+", ' ', text )
-    #linesList = sent_tokenize(text)
-    #return linesList
-    li = []
-    for k,v in var.items():
-        wordSet = set(v)
-        for everyLine in new_text:
-            wordsLine = everyLine.split(' ')
-            wordsLineSet = set(wordsLine)
-            #output_dict["skills"] = new_text
-            jaccard_score = float(len(wordsLineSet.intersection(wordSet)))/(len(wordsLineSet.union(wordSet))+1)
-            #li.append(jaccard_score)
-            if jaccard_score>=0.007:
-                new_output_dict[k].append(everyLine)
-
     stopWordSet = set(stopwords.words('english'))
     tokens = word_tokenize(cleanString)
 
     #keywords = [word for word in tokens if not word in stop_words]
-    #output_dict["skills"] = li
-    #ACTUAL METHOD
-    #for k,v in var.items():
-    #    new_set = set(v).intersection(set(tokens))
-    #    output_dict[k] = list(new_set)
+
+    for key in keys:
+        for word in tokens:
+            if word not in stopWordSet:
+                data[key].append(word)
 
     #jsonData = json.dumps(data)
 
@@ -76,8 +52,7 @@ def ConvertPdfToText(file):
     #file = open('./static/fileText.json','w')
     #file.write(jsonData)
     #file.close()
-
-    return new_output_dict
+    return data
 
 #output = ConvertPdfToText('./static/test.pdf')
 #jsonData = json.dumps(output)
