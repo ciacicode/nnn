3bacb2c22f7499992d4184b4aabda47eda336bad
Maria Cerase <maria.cerase@gmai.com>
2018-07-14 10:09:13 +0530

    updated requirements.txt and now got a working resume result page

--- app/app/views/__init__.py
+++ app/app/views/__init__.py
@@ -5,6 +5,8 @@ from .pdftotext import ConvertPdfToText
 from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
 from .resume_form import Resume
+import pdb
+import json
 
 bp = Blueprint('views', __name__, template_folder='templates')
 MYDIR = os.path.dirname(__file__)
@@ -20,14 +22,11 @@ def show():
 def unbias():
     form = Resume(request.form)
     if form.validate_on_submit():
-        # show resume_result
-        #unbiased = {"id": 0,"skills": [ "Python", "Css"],"experience": ["Google", "Import.io"],"education": ["University of Federico II, Naples"]}
+        # get resume as file from the form
         file = request.files['resume']
-        #file is a binary from the post request
-        #filename = secure_filename(file.filename)
+        #file is stored in a folder
         file.save(os.path.join(MYDIR+'/static/', file.filename))
         unbiased = ConvertPdfToText(MYDIR+'/static/'+file.filename)
-
         return render_template('resume_result.html', result=unbiased)
     else:
         #it's a get request
@@ -38,11 +37,11 @@ def diversity():
     form = Resume(request.form)
     if form.validate_on_submit():
         # show resume_result
-        # call diversity_score functions
+        # call diversity_score functions from shraddha's code
         # store in insights
         insights = [{"candidate":{"personality": {"Dominance": 0.8, "Influence":0.4, "Conscientiousness":0.7,"Steadiness":0.9 }}},
         {"team": {"personality": {"Dominance":0.4, "Influence":0.7, "Conscientiousness":0.4, "Steadiness":0.7 }}}]
-        return render_template('diversity_result.html', insights=insights)
+        return render_template('diversity_result.html', insights=json.dumps(insights))
     else:
         #it's a get request
         return render_template('diversity_upload.html', form=form)
