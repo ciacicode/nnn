6d64f104e08274ccd42048a46f9b45f86d430ed0
Maria <ciacicode@users.noreply.github.com>
2020-06-06 09:56:22 +0100

    removal

--- app/app/config.py
+++ app/app/config.py
@@ -8,8 +8,8 @@ Created on Fri Jul 13 19:43:34 2018
 class Config(object):
     WATSON = {
             "url": "https://gateway.watsonplatform.net/personality-insights/api",
-            "username": "46e9f51f-22bd-4972-91f5-4d8a9f89f1b2",
-            "password": "vT6J6CZMON2G",
+            "username": "",
+            "password": "",
             "limit" : "300"
 }
     SECRET_KEY = "notsosecret"
